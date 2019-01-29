0ab959a9b892c04256da6156e6f755eb5353d258
James Gregor <zc.sgs5@gmail.com>
2019-01-28 20:52:12 -0500

    Add webpack and babel for transpiling react code Add conditional selection of react libraries based on environment

---
+++ Webtoys.Web/webpack.config.js
@@ -0,0 +1,55 @@
+﻿/// <binding ProjectOpened='Watch - Development' />
+const path = require('path');
+
+module.exports = [
+    {
+        entry: {
+            client: './Components/site.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'wwwroot/js'),
+            filename: 'site.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                }
+            ]
+        },
+        externals: {
+            react: 'React',
+            'react-dom': 'ReactDOM'
+        },
+        mode: 'development'
+    },
+    {
+        entry: {
+            client: './Components/site.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'wwwroot/js'),
+            filename: 'site.min.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                }
+            ]
+        },
+        externals: {
+            react: 'react',
+            'react-dom': 'react-dom'
+        },
+        mode: 'production'
+    }
+];
